1a1120a0df1d27f47135ef240a02d23e7108dba2
t02ky <158488298+t02ky@users.noreply.github.com>
2024-04-26 11:50:03 +0900

    Update script.js
    リダイレクト先のページを表示されないようにした。

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 /*ここにjava Scriptを格納する*/
 window.location.href="https://t02ky.github.io/redirect/index.html";
+/*現段階ではリダイレクト先のページを表示させたくないので、mywebpageの内容を表示させるコード*/
+window.location.href="https://t02ky.github.io/mywebpage/index.html";
